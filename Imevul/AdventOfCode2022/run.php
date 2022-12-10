21c1dfa26c5cfd23f7c3eb93e68adb6e4c252083
Jonas Söderberg <imevul@gmail.com>
2022-12-10 05:47:42 +0100

    * Updated run code

--- Imevul/AdventOfCode2022/run.php
+++ Imevul/AdventOfCode2022/run.php
@@ -3,6 +3,7 @@
 namespace Imevul\AdventOfCode2022;
 
 ini_set('error_reporting', E_ALL ^ E_DEPRECATED);
+define('IS_CLI', php_sapi_name() == 'cli');
 
 use Console_Color2;
 use Console_Table;
@@ -10,11 +11,6 @@ use DateTime;
 use SebastianBergmann\Timer\Timer;
 
 require_once __DIR__ . '/../../bootstrap.php';
-
-$day = $argv[1] ?? NULL;
-
-define('IS_CLI', php_sapi_name() == 'cli');
-define('ONLY_LATEST', $day === NULL);
 
 function success(mixed $v, bool $color = TRUE): mixed {
 	return $color ? (new Console_Color2)->convert("%G$v%n") : $v;
@@ -24,7 +20,7 @@ function failure(mixed $v, bool $color = TRUE): mixed {
 	return $color ? (new Console_Color2)->convert("%R$v%n") : $v;
 }
 
-function runDay(int $day): array {
+function runDay(int $day): ?array {
 	$filename = "Day$day/day$day.php";
 
 	if (file_exists($filename)) {
@@ -35,41 +31,46 @@ function runDay(int $day): array {
 		return [$day, ...$result, $duration->asString()];
 	}
 
-	return [$day, [], ''];
+	return NULL;
 }
 
-$days = [];
-$directories = glob('Day*');
-natsort($directories);
-$maxDay = (int)str_replace('Day', '', $directories[array_key_last($directories)]);
+function runDays(mixed $day): void {
+	$days = [];
+	$directories = glob('Day*');
+	natsort($directories);
+	$maxDay = (int)str_replace('Day', '', $directories[array_key_last($directories)]);
 
-if (ONLY_LATEST) {
-	$day = $maxDay;
-}
+	$day ??= $maxDay;
 
-if ($day >= 1 && $day <= $maxDay) {
-	$days[$day] = runDay($day);
-} else {
-	for ($i = 1; $i <= $maxDay; $i++) {
-		$days[$i] = runDay($i);
+	if ($day == 'all') {
+		for ($i = 1; $i <= $maxDay; $i++) {
+			$days[$i] = runDay($i);
+		}
+	} else {
+		$result = runDay($day);
+		if ($result !== NULL) {
+			$days[$day] = $result;
+		}
 	}
+
+	$data = [];
+	foreach ($days as $day) {
+		$data[] = [
+			$day[1][0] && $day[1][1] ? success($day[0], IS_CLI) : failure($day[0], IS_CLI),
+			$day[1][0] ? success($day[2][0], IS_CLI) : failure($day[2][0], IS_CLI),
+			$day[1][1] ? success($day[2][1], IS_CLI) : failure($day[2][1], IS_CLI),
+			$day[3],
+		];
+	}
+
+	$table = new Console_Table(color: IS_CLI);
+	$table->setHeaders(['Day', 'Part1', 'Part2', 'Time']);
+	$table->addData($data);
+	$tableText = $table->getTable();
+	$tableText = IS_CLI ? $tableText : str_replace([' ', PHP_EOL], ['&nbsp;', '<br>'], $tableText);
+
+	echo (new DateTime('now'))->format('c') . PHP_EOL;
+	echo $tableText;
 }
 
-$data = [];
-foreach ($days as $day) {
-    $data[] = [
-        $day[1][0] && $day[1][1] ? success($day[0], IS_CLI) : failure($day[0], IS_CLI),
-        $day[1][0] ? success($day[2][0], IS_CLI) : failure($day[2][0], IS_CLI),
-        $day[1][1] ? success($day[2][1], IS_CLI) : failure($day[2][1], IS_CLI),
-        $day[3],
-    ];
-}
-
-$table = new Console_Table(color: IS_CLI);
-$table->setHeaders(['Day', 'Part1', 'Part2', 'Time']);
-$table->addData($data);
-$tableText = $table->getTable();
-$tableText = IS_CLI ? $tableText : str_replace([' ', PHP_EOL], ['&nbsp;', '<br>'], $tableText);
-
-echo (new DateTime('now'))->format('c') . PHP_EOL;
-echo $tableText;
+runDays($argv[1] ?? NULL);
